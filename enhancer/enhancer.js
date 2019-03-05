adfea865921a7ef04790549921d5e8bd72e029d7
rmejia-tajc <rmejia.tajc@gmail.com>
2019-03-04 21:58:10 -0700

    create fail test and method

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,15 +1,95 @@
+
+
+// Takes an item and return a new item with durability of a 100.
 exports.repair = (item) => {
-    //takes an item and return a new item with durability of a 100
     return { ...item, durability: 100};
 };
 
+
+// The item's enhancement increases by 1. Max 20.
+// The name is updated to reflect the new enhancement level.
 exports.success = (item) => {
     const enhanceUp = item.enhancement + 1;
-    // const updatedName = 'X';
+    const updatedName = `[+${enhanceUp}] ${item.baseName}`;
 
+    if(enhanceUp >= 20) {
+        return {
+            ...item,        
+            enhancement: 20,
+            displayName: `[PEN] ${item.baseName}`
+            };
+    } 
+    
+    if(enhanceUp === 19) {
+        return {
+            ...item,        
+            enhancement: 19,
+            displayName: `[TET] ${item.baseName}`
+        };
+    } 
+    
+    if(enhanceUp === 18) {
+        return {
+            ...item,        
+            enhancement: 18,
+            displayName: `[TRI] ${item.baseName}`
+        };
+    } 
+    
+    if(enhanceUp === 17) {
+        return {
+            ...item,        
+            enhancement: 17,
+            displayName: `[DUO] ${item.baseName}`
+        };
+    } 
+    
+    if(enhanceUp === 16) {
+        return {
+            ...item,        
+            enhancement: 16,
+            displayName: `[PRI] ${item.baseName}`
+        };
+    } 
+    
+    if(enhanceUp === 0) {
+        return {
+            ...item,        
+            enhancement: 0,
+            displayName: `${item.baseName}`
+        };
+    } else {
+    
     return {
         ...item,        
         enhancement: enhanceUp,
-        // displayName: updatedName
-    };
+        displayName: updatedName,
+        };
+    }
+};
+
+
+// The durability of the item is decreased by 5 if the item's enhancement is between 0 and 14.
+// The durability of the item is decreased by 10 if the item's enhancement is greater than 14.
+// If the item's enhancement level is greater than 16 (DUO, TRI, TET), the enhancement level decreases by 1 (a DUO item would go back to PRI on failure).
+// The name is updated to reflect the new enhancement level if it was decreased.
+// If the item's enhancement is 14 or lower, the item cannot be enhanced if the durability is below 25.
+// If the item's enhancement is 15 or higher, the item cannot be enhanced if the durability is below 10.
+exports.fail = (item) => {
+    if (item.enhancement < 15 && item.durability < 25) {
+      return { ...item };
+    }
+
+    if (item.enhancement > 14 && item.durability < 10) {
+      return { ...item };
+    }
+  
+    const durability =
+      item.enhancement < 15 ? item.durability - 5 : item.durability - 10;
+  
+    const enhancement =
+      item.enhancement > 16 ? item.enhancement - 1 : item.enhancement;
+  
+    return { ...item, durability, enhancement };
+
 };
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,4 +1,4 @@
-const { repair, success } = require('./enhancer.js');
+const { repair, success, fail } = require('./enhancer.js');
 
 
 describe('enhancer.js', () => {
@@ -14,10 +14,10 @@ describe('enhancer.js', () => {
 
     const testItem02 = {
         baseName: 'Redux Cloak',
-        displayName: '[TET] Redux Cloak',
+        displayName: '[PEN] Redux Cloak',
         type: 'armor',
         durability: 5,
-        enhancement: 19,
+        enhancement: 20,
     };
 
     const testItem03 = {
@@ -45,10 +45,24 @@ describe('enhancer.js', () => {
         const actual03 = success(testItem03);
         const actual04 = success(testItem04);
 
-        it('should increase the item\'s enhancement +1. Max 20', () => {
+        // The item's enhancement increases by 1. Max 20
+        it('should increase the item\'s enhancement +1', () => {
 
             //assert
             expect(actual01.enhancement).toBe(8); // assertion
+            expect(actual02.enhancement).toBe(20); // assertion
+            expect(actual03.enhancement).toBe(18); // assertion
+            expect(actual04.enhancement).toBe(1); // assertion
+        });
+
+        // The name is updated to reflect the new enhancement level
+        it('should update name to ([enhancement level] name)', () => {
+
+            //assert
+            expect(actual01.displayName).toBe('[+8] Lambda Shield'); // assertion
+            expect(actual02.displayName).toBe('[PEN] Redux Cloak'); // assertion
+            expect(actual03.displayName).toBe('[TRI] Sword of Javascript'); // assertion
+            expect(actual04.displayName).toBe('[+1] CSS Stone'); // assertion
         });
 
 
@@ -74,6 +88,72 @@ describe('enhancer.js', () => {
             expect(actual03.durability).toBe(100); // assertion
             expect(actual04.durability).toBe(100); // assertion
         });
+
+    });
+    
+
+    describe('fail() method', () => {
+
+        // Act - run SUT (System Under Test)
+        const actual01 = fail(testItem01);
+        const actual02 = fail(testItem02);
+        const actual03 = fail(testItem03);
+        const actual04 = fail(testItem04);
+
+
+
+        // The durability decreased by 5 if the item's enhancement is between 0 and 14.
+        it('durability decreases by 5 if enhancement is between 0 and 14', () => {
+          // Arrange (setup)
+          const item = {
+            enhancement: 14,
+            durability: 85,
+          };
+     
+          // Act - execute the SUT
+          const actual = fail(item);
+     
+          // Assert
+          expect(actual.durability).toBe(80);
+        });
+     
+        it('durability decreases by 10 if enhancement is greater than 14', () => {
+          // Assert
+          expect(
+            fail({
+              enhancement: 15,
+              durability: 85,
+            }).durability
+          ).toBe(75);
+        });
+     
+        it('should not affect item if enhancement is less than 15 and the durability is below 25', () => {
+          const item = {
+            enhancement: 14,
+            durability: 24,
+          };
+     
+          expect(fail(item)).toEqual(item);
+        });
+     
+        // enhancement greater than 16 > enhancement decreases by 1
+        it('should decrease enhancement if item is enhanced greater than 16', () => {
+          const item = { enhancement: 18 };
+     
+          const actual = fail(item);
+     
+          expect(actual.enhancement).toBe(17);
+        });
+     
+        it('should not affect item if enhancement is more than 15 and the durability is below 10', () => {
+          const item = {
+            enhancement: 16,
+            durability: 9,
+          };
+     
+          expect(fail(item)).toEqual(item);
+        });
+
 
     });
 
